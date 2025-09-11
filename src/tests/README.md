82b0dbd6afb40dc61d87fd7682015e8eb701b2fa
PequeKiki <orellanaabregoeric@gmail.com>
2025-09-10 21:29:39 -0400

    Tests pour calculator.py

---
+++ src/tests/README.md
@@ -0,0 +1,2 @@
+# Les tests
+Pour lancer les tests, il faut faire "pytest" dans la racine du projet. Il y a 6 tests qui manchent et 1 qui échouent pour tester la réaction de l'échec.
--- src/tests/test_calculator.py
+++ src/tests/test_calculator.py
@@ -4,10 +4,37 @@ SPDX - License - Identifier: LGPL - 3.0 - or -later
 Auteurs : Gabriel C. Ullmann, Fabio Petrillo, 2025
 """
 
+import pytest
 from calculator import Calculator
 
 def test_app():
     my_calculator = Calculator()
-    assert my_calculator.get_hello_message() == "Calculatrice"
+    assert my_calculator.get_hello_message() == "== Calculatrice v1.0 =="
 
 # TODO: ajoutez les tests
+
+def test_addition():
+    c = Calculator()
+    assert c.addition(2, 3)
+
+def test_subtraction():
+    c = Calculator()
+    assert c.subtraction(10, 4) == 6
+
+def test_multiplication():
+    c = Calculator()
+    assert c.multiplication(7, 6) == 42
+
+def test_division():
+    c = Calculator()
+    assert c.division(9, 2) == 4.5
+
+def test_division_by_0():
+    c = Calculator()
+    assert c.division(9, 0) == "Erreur : division par zéro"
+
+# Test qui échoue
+#def test_echec_addition():
+#    c = Calculator()
+#    assert c.addition(2, 2) == 5
+#
